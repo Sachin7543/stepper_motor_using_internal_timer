336c1daf6cf190564efaa834cdf700faf275286b
Sachin Patil <56131842+Sachin7543@users.noreply.github.com>
2020-06-02 14:01:11 +0530

    Update stepper_motor_using_internal_timer.ino

--- stepper_motor_using_internal_timer.ino
+++ stepper_motor_using_internal_timer.ino
@@ -1,7 +1,6 @@
 #define dirPin 51
 #define stepPin 49
-int state = 10000;
-int i = 0;
+int state = 0;
 void setup()
 {
   pinMode(stepPin, OUTPUT);
@@ -17,23 +16,26 @@ void setup()
   TCCR0B |= (1 << CS00); 
   TIMSK0 = TIMSK0 |(1 << OCIE0A);  // enable timer compare interrupt
   sei();             // enable all interrupts
-  Serial.begin(9600);
 
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1  = 0;
+  OCR1A  = 30;       // compare match register 16MHz/256/2Hz
+  TCCR1B = TCCR1B | (1 << WGM12);  // CTC mode
+  TCCR1B = TCCR1B | ((1 << CS12) | (1 << CS10)); // 256 prescaler
+  TIMSK1 = TIMSK1 | (1 << OCIE1A); // enable timer compare interrup
+  Serial.begin(9600);
 }
 void loop()
 {
    digitalWrite(dirPin, HIGH);
-   if ( i  < state)
-   {
-      digitalWrite(stepPin, LOW); 
-   }
-   else
-   {
-    i = 0;
-   }
 }
 
 ISR(TIMER0_COMPA_vect)          // timer compare interrupt service routine
+{ 
+  digitalWrite(stepPin, HIGH);
+}
+ISR(TIMER1_COMPA_vect)          // timer compare interrupt service routine
 {
-  i++;          
+   digitalWrite(stepPin, LOW);
 }
